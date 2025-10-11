4123f7a577dfc01c7f3adabc9812833e27aacde1
Ananyaa Mylsamy <ananyaamylsamy@Ananyaas-MacBook-Pro.local>
2025-10-11 17:36:20 -0400

    Fix CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
+    origin: true,
     credentials: true,
   })
 );
